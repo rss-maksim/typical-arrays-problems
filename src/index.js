ec68c659a163118f71aff587f13feabf82bc9fc0
maksim.kirilko <maksim.kirilko@gmail.com>
2020-02-27 12:45:00 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+  if (!array || !array.length) {
+      return 0;
+  }
+  return array.reduce((min, number) => number < min ? number : min, 0);
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (!array || !array.length) {
+        return 0;
+    }
+    return array.reduce((max, number) => number > max ? number : max, 0);
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+  if (!array || !array.length) {
+      return 0;
+  }
+  const sum = array.reduce((acc, number) => acc + number, 0);
+  return sum / array.length
+};
